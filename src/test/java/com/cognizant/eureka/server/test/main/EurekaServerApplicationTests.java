c70a5de642d8dbaacae1bbab9945aa9237c9f25c
Adrineel-Saha <adrineelsaha@gmail.com>
2026-01-28 17:05:39 +0530

    Added Code for Eureka Server

---
+++ src/test/java/com/cognizant/eureka/server/test/main/EurekaServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.cognizant.eureka.server.test.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.cognizant.eureka.server.main.EurekaServerApplication;
+
+@SpringBootTest(classes=EurekaServerApplication.class)
+public class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
